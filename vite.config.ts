1e3fa62272d58add43d70d6b5068ab64e7e20e54
bima sandika <bimasandika@gmail.com>
2026-09-05 00:38:19 +0700

    fix: set base path to /admin/ for nginx

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/admin/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
