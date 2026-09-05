a12f3d836f3d7b69329ed353e9bc34584481bb78
bima sandika <bimasandika@gmail.com>
2026-09-05 18:10:09 +0700

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/admin/",
+  base: "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
